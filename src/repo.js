da1a889a39c447ed3bdfa8ca43818620b9ec2a5d
SokuRitszZ <1062708758@qq.com>
2023-05-15 15:27:00 +0800

    feat: chrome extension + solidjs template

--- src/repo.js
+++ src/repo.js
@@ -3,4 +3,6 @@ export default {
     'https://github.com/SokuRitszZ/vite-solidjs-ts-router-template',
   'Electron + Vite + Solid + TS':
     'https://github.com/SokuRitszZ/electron-vite-solid-ts-template',
+  'Chrome Extension + Vite + Solid + TS':
+    'https://github.com/SokuRitszZ/chrome-vite-solid-ts-template',
 };
